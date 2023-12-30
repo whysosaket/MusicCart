e9ba4ea1bb855ebd2d8172ad6702e8a56d07e8b4
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-12-30 20:00:25 +0530

    Added Checkout Page

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,51 +1,79 @@
-import React, { useEffect } from 'react'
-import "../css/Checkout.css"
-import CheckoutDeliveryAddress from '../components/Checkout/CheckoutDeliveryAddress'
-import CheckoutPayment from '../components/Checkout/CheckoutPayment'
-import CheckoutReviewItems from '../components/Checkout/CheckoutReviewItems'
-import CheckOutMiniOrder from '../components/Checkout/CheckOutMiniOrder'
-import PlaceOrderWidget from '../components/Checkout/PlaceOrderWidget'
-import { Link, useParams } from 'react-router-dom'
-import CartContext from '../context/cartContext';
-import { useContext } from 'react';
+import React, { useEffect } from "react";
+import "../css/Checkout.css";
+import CheckoutDeliveryAddress from "../components/Checkout/CheckoutDeliveryAddress";
+import CheckoutPayment from "../components/Checkout/CheckoutPayment";
+import CheckoutReviewItems from "../components/Checkout/CheckoutReviewItems";
+import CheckOutMiniOrder from "../components/Checkout/CheckOutMiniOrder";
+import PlaceOrderWidget from "../components/Checkout/PlaceOrderWidget";
+import { Link, useParams } from "react-router-dom";
+import CartContext from "../context/cartContext";
+import { useContext } from "react";
+import { IoMdArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const Checkout = (props) => {
-  const {cart, total, checkout, buyNow} = useContext(CartContext);
-  const {id} = useParams();
+  const { cart, total, checkout, buyNow } = useContext(CartContext);
+  const { id } = useParams();
 
-  const dummyCheckout = () => {
-    
-  };
+  const dummyCheckout = () => {};
 
   useEffect(() => {
-    if(props.buyNow){
+    if (props.buyNow) {
       buyNow(id);
     }
-  }, [])
-
+  }, []);
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    if (total === 0) {
+      navigate("/");
+      return;
+    }
+    checkout();
+    navigate("/success");
+  };
 
   return (
     <div className="checkout">
       <div className="goback">
-        <Link to="/cart" className="gobackbtn">Back to cart</Link>
+        <Link to="/cart" className="gobackbtn">
+          <IoMdArrowBack className="gobackicon" />
+          <span className="pc">Back to Products</span>
+        </Link>
       </div>
-      <div className='title'>Checkout</div>
+      <div className="title">Checkout</div>
       <div className="checkoutdiv">
         <div className="checkoutleft">
           <CheckoutDeliveryAddress />
           <hr />
-          <CheckoutPayment  />
+          <CheckoutPayment />
           <hr />
           <CheckoutReviewItems cart={cart} />
           <hr />
-          <CheckOutMiniOrder total={total} />
+          <div className="pc">
+            <CheckOutMiniOrder total={total} />
+          </div>
         </div>
-        <div className="checkoutright">
-          <PlaceOrderWidget total={total} checkout={props.buyNow?dummyCheckout:checkout} />
+        <div className="mobile">
+          <hr />
+          <div className="">
+            <div className="leftitem">
+              <div className="ll">Order Total: ₹{total}</div>
+            </div>
+          </div>
+          <div className="placeorderwidget">
+            <button onClick={handleCheckout}>Place your order</button>
+          </div>
+        </div>
+
+        <div className="checkoutright pc">
+          <PlaceOrderWidget
+            total={total}
+            checkout={props.buyNow ? dummyCheckout : checkout}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
